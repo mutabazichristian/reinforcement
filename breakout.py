83757db319024a669b74274d93063044676fb082
Lindah <l.nyambura@alustudent.com>
2025-07-09 16:17:54 +0300

    atari env practice code

--- breakout.py
+++ breakout.py
@@ -1,14 +1,20 @@
 import gymnasium as gym
 
-env = gym.make('Skiing-v5', render_mode='human')
-obs, info = env.reset()
-done = False
-#array
-while not done:
-    action = env.action_space.sample()  
-    obs, rew, done, truncated, info = env.step(action)
-    #record obs, rew, info
-    if done or truncated:
-        obs, info = env.reset()
-    
-env.close()
+env = gym.make('FrozenLake-v1', render_mode='human')   
+obs, info = env.reset()                           
+
+max_steps = 20                                     
+terminated = False
+truncated = False
+
+for step in range(max_steps):                    
+    action = env.action_space.sample()             
+    obs, reward, terminated, truncated, info = env.step(action)  
+
+    print(f"Step: {step + 1}, Action: {action}, Reward: {reward}")
+
+    if terminated or truncated:                   
+        print("Episode ended.")                  
+        break
+
+env.close()                                        
